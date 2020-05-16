0f217733274c2ec3c520504961dda6ee58da338b
Alfredo Jose <alfredo@frncubo.com>
2020-05-16 16:40:29 -0300

    build(src): deleted some files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import graphql from 'babel-plugin-relay/macro';
+import { QueryRenderer } from 'react-relay';
 
-function App() {
+import { Environment } from './relay';
+
+const App = ({ query }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <span>me: ${query.me && query.me.name}</span>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Learn Relay
         </a>
       </header>
     </div>
   );
-}
+};
 
-export default App;
+const AppQR = () => {
+  return (
+    <QueryRenderer
+      environment={Environment}
+      query={graphql`
+        query AppQuery {
+          me {
+            id
+            name
+          }
+        }
+      `}
+      variables={{}}
+      render={({ error, props }) => {
+        console.log('qr: ', error, props);
+        if (error) {
+          return <span>{error.toString()}</span>;
+        }
+
+        if (props) {
+          return <App query={props} />;
+        }
+
+        return <span>loading</span>;
+      }}
+    />
+  );
+};
+
+export default AppQR;
