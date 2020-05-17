bf87311960c6f6d640c2f57b2651d87e2a630bc4
Alfredo Jose <alfredo@frncubo.com>
2020-05-17 00:40:34 -0300

    builld(app): now getting a post by id

--- src/App.js
+++ src/App.js
@@ -1,59 +1,41 @@
 import React from 'react';
+import { createFragmentContainer } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
-import { QueryRenderer } from 'react-relay';
+import createQueryRenderer from './relay/createQueryRendererModern';
 
-import { Environment } from './relay';
+const Post = ({ Post }) => (
+  <div>
+    <span>{Post.title}</span>
+    <span>{Post.text}</span>
+  </div>
+);
 
-const App = ({ query }) => {
-  const listItems = query.allUsers.map(item => (
-    <li key={item.id}>{item.name}</li>
-  ));
-  return (
-    <div className='App'>
-      <header className='app-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>me: ${listItems}</span>
-        <a
-          className='app-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn Relay
-        </a>
-      </header>
-    </div>
-  );
-};
+const PostFragmentContainer = createFragmentContainer(Post, {
+  Post: graphql`
+    fragment App_post on Post {
+      id
+      title
+      text
+    }
+  `,
+});
 
-const AppQR = () => {
-  return (
-    <QueryRenderer
-      environment={Environment}
-      query={graphql`
-        query AppQuery {
-          allUsers {
-            id
-            name
-          }
-        }
-      `}
-      variables={{}}
-      render={({ error, props }) => {
-        console.log('qr: ', error, props);
-        if (error) {
-          return <span>{error.toString()}</span>;
-        }
-
-        if (props) {
-          return <App query={props} />;
-        }
-
-        return <span>loading</span>;
-      }}
-    />
-  );
-};
+const AppQR = createQueryRenderer(PostFragmentContainer, Post, {
+  query: graphql`
+    query AppQuery($id: ID!) {
+      Post(id: $id) {
+        ...App_post
+      }
+    }
+  `,
+  queriesParams: () => ({
+    id: 'ck2hdh20m0va70135cptz2jn4',
+  }),
+  getFragmentProps: ({ Post }) => {
+    return {
+      Post,
+    };
+  },
+});
 
 export default AppQR;
